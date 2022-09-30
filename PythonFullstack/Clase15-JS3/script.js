3e1618aa2530457fdb1d808967da7e8ddca3db5a
Martin Lopez <martinlg2907@gmail.com>
2022-09-30 18:02:41 -0300

    Clase 15

---
+++ PythonFullstack/Clase15-JS3/script.js
@@ -0,0 +1,61 @@
+console.log("While")
+
+let contador = 1;
+
+while (contador <= 10) {
+    console.log(contador);
+    contador++;
+}
+
+for (let contador = 1; contador < 10; contador++) {
+    console.log(contador);
+}
+
+console.log("For");
+
+let num = parseInt(prompt("Agregue un num"));
+
+for (let i = 0; i <= num; i++) {
+    if(i%2==0){
+        console.log(i);
+    }
+}
+
+console.log("Array");
+
+let newArray = ["cristian","maria",1,2,3,4,true,false];
+console.log(newArray);
+
+let newArray2 = new Array("juan", "seba");
+
+let arrayVacio = new Array();
+arrayVacio[0] = "juan";
+arrayVacio[1] = "maria";
+arrayVacio[2] = "pedro";
+console.log(arrayVacio); //arrayVacio.length tamaño del array
+//arrayVacio[0] --> posicion dentro del array
+
+arrayVacio[arrayVacio.length] = "jorge"; //arrayVacio.push("jorge");
+
+arrayVacio.unshift("emi");//agrega al principio
+
+arrayVacio.shift();//elimina el primer elemento
+
+arrayVacio.pop();//elimina el ultimo elemento
+
+console.log(arrayVacio.reverse());//invierte el array
+console.log(arrayVacio.join("-"));//muestra los elementos con alguna condicion particular
+
+//---------------------------- O ----------------------------------//
+
+let unArray = [1,2,3]
+let otroArray = [4,5,6]
+
+let nuevoArray = unArray.concat(otroArray);
+console.log(nuevoArray);
+
+//let nuevoArray = [].concat(unArray, nuevoArray);
+//console.log(nuevoArray);
+
+
+
